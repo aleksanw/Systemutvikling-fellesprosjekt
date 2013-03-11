0603cf8b0944a1acc179e05aba328f9b2395ddb2
Mads Paulsen <madslp@stud.ntnu.no>
2013-03-11 12:03:33 +0100

    Navnendringer

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Model {
 
+	private String primaryKeyField1, primaryKeyField2;
+	private ArrayList<String> tableFields;
+	private String tableName;
 }
--- src/model/RoomModel.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public class RoomModel extends Model {
-
-}
