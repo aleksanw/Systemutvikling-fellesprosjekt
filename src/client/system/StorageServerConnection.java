59c8bae48d2b8f5f5d141a4afd7a5da638e262e5
Jon Tingvold <jon.tingvold@gmail.com>
2013-03-14 14:34:08 +0100

    Update StorageClientConnection fix

--- src/client/system/StorageServerConnection.java
+++ src/client/system/StorageServerConnection.java
@@ -20,21 +20,21 @@ public class StorageServerConnection {
 		/*
 		RMIClient server = new RMIClient();
 		
-		StorageI eventStorage = server.getClass("EventStorage");
-		StorageI userStorage = server.getClass("UserStorage");
-		StorageI groupStorage = server.getClass("GroupStorage");
-		StorageI roomStorage = server.getClass("RoomStorage");
-		StorageI alarmStorage = server.getClass("AlarmStorage");
-		StorageI invitationStorage = server.getClass("InvitationStorage");
+		this.eventStorage 		= server.getClass("EventStorage");
+		this.userStorage 		= server.getClass("UserStorage");
+		this.groupStorage 		= server.getClass("GroupStorage");
+		this.roomStorage 		= server.getClass("RoomStorage");
+		this.alarmStorage 		= server.getClass("AlarmStorage");
+		this.invitationStorage 	= server.getClass("InvitationStorage");
 		*/
 		
 		
 		// Dummy server
-		this.eventStorage = new Storage(Event.class);
-		this.userStorage = new Storage(User.class);
-		this.groupStorage = new Storage(Group.class);
-		this.roomStorage = new Storage(Room.class);
-		this.alarmStorage = new Storage(Alarm.class);
-		this.invitationStorage = new Storage(Invitation.class);
+		this.eventStorage 		= new Storage(Event.class);
+		this.userStorage 		= new Storage(User.class);
+		this.groupStorage 		= new Storage(Group.class);
+		this.roomStorage 		= new Storage(Room.class);
+		this.alarmStorage 		= new Storage(Alarm.class);
+		this.invitationStorage 	= new Storage(Invitation.class);
 	}
 }
