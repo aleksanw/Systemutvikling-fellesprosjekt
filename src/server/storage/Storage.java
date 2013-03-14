b3e3e1ba18b2bc65d3117d5843629447e69e388b
Jon Tingvold <jon.tingvold@gmail.com>
2013-03-14 14:08:33 +0100

    Delete unused Storages

--- src/server/storage/Storage.java
+++ src/server/storage/Storage.java
@@ -4,7 +4,6 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Hashtable;
 
-import common.EventStorageI;
 import common.StorageI;
 
 import server.model.*;
