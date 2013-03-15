7a993cd6a1052619ece207d4993101654c2f0386
Johannes Lier <johannes.o.lier@gmail.com>
2013-03-15 14:42:15 +0100

    listmodel

---
+++ src/server/model/ListModel.java
@@ -0,0 +1,17 @@
+package server.model;
+
+import java.beans.PropertyChangeSupport;
+
+import javax.swing.DefaultListModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class ListModel<E> implements ListSelectionListener{
+	
+	PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+	private DefaultListModel<E> model;
+
+	public void valueChanged(ListSelectionEvent e) {
+		System.out.println("Hei");
+	}
+}
