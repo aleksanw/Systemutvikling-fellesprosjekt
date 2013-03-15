e2fe6d4d03ff5aa53704bd08a28d13741432a1b4
Mads Paulsen <madslp@stud.ntnu.no>
2013-03-16 00:52:28 +0100

    test

--- src/server/model/MainSystem.java
+++ src/server/model/MainSystem.java
@@ -9,12 +9,9 @@ public class MainSystem {
 	
 	public static void main(String[] args) throws SQLException {
 		
-		Event a = new Event();
-		a.setEventName("Masse kake i dage");
-		a.setStart(new DateTime(2013,03,14,13,37,50,003));
-		a.setWholeDay(true);
+		Event a = new Event(2);
 		//DateTime dt = new DateTime(2014, 11, 20, 12, 15, 0, 0);
 		//DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:SS");
-		System.out.println(a.getEventID());
+		System.out.println(a.getEventName());
 	}
 }
