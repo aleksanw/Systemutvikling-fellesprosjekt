295f83be75465d5af4e834c331564408a587558c
Jon Tingvold <jon.tingvold@gmail.com>
2013-03-18 16:31:35 +0100

    Alarm relate to objects, not IDs + Add RemoteException

--- src/common/AlarmI.java
+++ src/common/AlarmI.java
@@ -1,25 +1,27 @@
 package common;
 
+import java.rmi.RemoteException;
 import java.sql.SQLException;
 import java.sql.Time;
 
 public interface AlarmI extends ModelI {
 
-	public abstract int getAlarmID();
+	public abstract int getAlarmID() throws RemoteException;
 
-	public abstract int getUserID();
+	public abstract UserI getUser() throws RemoteException;
 
-	public abstract int getEventID();
+	public abstract EventI getEvent() throws RemoteException;
 
-	public abstract Time getNumberOfHoursBeforeMeeting();
+	public abstract Time getNumberOfHoursBeforeMeeting() throws RemoteException;
 
-	public abstract void setUserID(int userID) throws SQLException;
+	public abstract void setUserID(int userID) throws SQLException, RemoteException;
+	public abstract void setUser(UserI user) throws SQLException, RemoteException;
 
-	public abstract void setEventID(int eventID) throws SQLException;
+	public abstract void setEventID(int eventID) throws SQLException, RemoteException;
+	public abstract void setEvent(EventI event) throws SQLException, RemoteException;
 
-	public abstract void setNumberOfHoursBeforeMeeting(
-			Time numberOfHoursBeforeMeeting) throws SQLException;
+	public abstract void setNumberOfHoursBeforeMeeting(Time numberOfHoursBeforeMeeting) throws RemoteException, SQLException;
 
-	public abstract void delete();
+	public abstract void delete() throws RemoteException;
 
 }
