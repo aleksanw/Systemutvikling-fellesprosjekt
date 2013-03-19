588e88e4a762047cc4c0a14b18f78879cfd5f34e
Jon Tingvold <jon.tingvold@gmail.com>
2013-03-19 12:01:27 +0100

    Interface for Week object

---
+++ src/common/WeekI.java
@@ -0,0 +1,12 @@
+package common;
+
+public interface WeekI {
+	public void WeekI(int year, int weeknr);
+	public void setWeek(int year, int weeknr);
+	public String getWeek();
+	public int getYear();
+	public int getWeeknr();
+	public String toString();
+	public WeekI getNextWeek();
+	public WeekI getPreviousWeek();
+}
