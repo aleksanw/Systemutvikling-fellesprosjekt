950b7fe067c9456bab7bc084d35773eb321bb0e0
Mads Paulsen <madslp@stud.ntnu.no>
2013-03-19 14:16:56 +0100

    it works kindoff

--- src/client/gui/CellRenderer.java
+++ src/client/gui/CellRenderer.java
@@ -1,18 +1,25 @@
 package client.gui;
 
 import java.awt.Component;
+import java.rmi.RemoteException;
 
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
-import server.model.Room;
+import common.RoomI;
 
-public class CellRenderer extends JLabel implements ListCellRenderer{
+public class CellRenderer extends JLabel implements ListCellRenderer {
 
 	public Component getListCellRendererComponent(JList list, Object value,
 			int index, boolean isSelected, boolean cellHasFocus) {
-		String s = ((Room) value).getRoomName();
+		String s;
+		try {
+			s = ((RoomI) value).getRoomName();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		}
 		setText(s);
 		return this;
 	}
