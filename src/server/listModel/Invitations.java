e6e203ad070c3185307c2afa43b544d2b2425606
Mads Paulsen <madslp@stud.ntnu.no>
2013-03-19 12:19:43 +0100

    invites okok

--- src/server/listModel/Invitations.java
+++ src/server/listModel/Invitations.java
@@ -12,26 +12,19 @@ import server.system.StorageServer;
 
 public class Invitations extends ListModel {
 	ArrayList<Event> invites = new ArrayList<Event>();
-	
-	public static void main(String[] args) throws RemoteException, SQLException {
-		Invitations i = new Invitations(new User(1));
-		ArrayList<Event> x = i.getList();
-		for(int y = 0; y< x.size() ; y++) {
-			System.out.println(x.get(y));
-		}
-	}
 
 	public Invitations(User user) throws SQLException, RemoteException {
 		int userID = user.getUserID();
-		String query = "SELECT * FROM InvitedTo natural join User where userID="+ userID +";";
+		String query = "SELECT * FROM InvitedTo natural join User where userID="
+				+ userID + ";";
 		ResultSet result = Model.getDB().readQuery(query);
-		while(result.next()) {
-			System.out.println(result.getInt("eventID"));
-			this.invites.add(StorageServer.eventStorage.get(result.getInt("eventID")));
+		while (result.next()) {
+			this.invites.add(StorageServer.eventStorage.get(result
+					.getInt("eventID")));
 		}
 	}
-	
-	public ArrayList<Event> getList() throws RemoteException{
+
+	public ArrayList<Event> getList() throws RemoteException {
 		return invites;
 	}
 }
