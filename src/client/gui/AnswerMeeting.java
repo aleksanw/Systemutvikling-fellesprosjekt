bc4a849e214271da7a3e1cb831328adb0126d6cf
markusandersson9 <markusandersson9@gmail.com>
2013-03-20 16:58:40 +0100

    AnswetMeeting

--- src/client/gui/AnswerMeeting.java
+++ src/client/gui/AnswerMeeting.java
@@ -5,34 +5,34 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import javax.swing.JScrollPane;
+
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import common.EventI;
+import common.InvitationI;
 
 class AnswerMeeting extends JPanel implements ActionListener {
 
 	private JLabel header, accept, decline;
 	private JLabel name = new JLabel();
 	private JLabel date = new JLabel();
-	private JLabel place = new JLabel();
 	private JLabel time = new JLabel();
-	private JLabel desc = new JLabel();
 	private JButton send, cancel;
 	private JRadioButton acc, dec;
-	private JScrollPane scroll;
 	private ButtonGroup myBGroup;
 	private EventI selectedEvent;
 
 	GridBagConstraints g = new GridBagConstraints();
 
-	public AnswerMeeting(String name, String date, String time, String desc,
-			String place) {
+	public AnswerMeeting(EventI event, InvitationI invite) {
 		header = new JLabel();
 		header.setText("Svar på innkalling");
 		header.setFont(new Font("Sans Serif", Font.PLAIN, 20));
@@ -54,11 +54,22 @@ class AnswerMeeting extends JPanel implements ActionListener {
 		dec = new JRadioButton();
 		dec.addActionListener(this);
 
-		this.name.setText(name);
-		this.date.setText(date);
-		this.time.setText(time);
-		this.desc.setText(desc);
-		this.place.setText(place);
+		try {
+			this.name.setText(event.getEventName());
+		} catch (RemoteException e2) {
+			throw new RuntimeException(e2);
+		}
+		DateTimeFormatter FMT = DateTimeFormat.forPattern("HH-mm dd-MM-yyyy");
+		try {
+			this.date.setText(FMT.print(event.getStart()));
+		} catch (RemoteException e1) {
+			throw new RuntimeException(e1);
+		}
+		try {
+			this.time.setText(FMT.print(event.getEnd()));
+		} catch (RemoteException e) {
+			throw new RuntimeException(e);
+		}
 
 		send = new JButton("Svar");
 		send.addActionListener(this);
@@ -89,27 +100,23 @@ class AnswerMeeting extends JPanel implements ActionListener {
 		g.gridy = 3;
 		add(time, g);
 
-		g.gridy = 4;
-		add(desc, g);
+//		g.gridy = 4;
+//		add(desc, g);
+//
+//		g.gridy = 5;
+//		add(place, g);
 
-		g.gridy = 5;
-		add(place, g);
-
-		g.gridy = 6;
-
-		g.gridy = 7;
-//		add(scroll, g);
-
+	
 		g.gridwidth = 1;
-		g.gridy = 8;
+		g.gridy = 4;
 		add(acc, g);
 		add(accept, g);
 
-		g.gridy = 9;
+		g.gridy = 5;
 		add(dec, g);
 		add(decline, g);
 
-		g.gridy = 10;
+		g.gridy = 6;
 		add(send, g);
 		
 		g.gridx = 1;
@@ -118,8 +125,7 @@ class AnswerMeeting extends JPanel implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().toString().equals("Svar")){
-//			Send svar til database og shit
-//			Add eventet til din kalender
+			
 			
 		}
 		else if (e.getActionCommand().toString().equals("Avbryt")){
