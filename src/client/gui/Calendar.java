cd1f666b29a691daa3170948ce93a8cac0aae5ce
Johannes Lier <johannes.o.lier@gmail.com>
2013-03-20 13:57:48 +0100

    Event i kalender

--- src/client/gui/Calendar.java
+++ src/client/gui/Calendar.java
@@ -5,19 +5,44 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
+import org.joda.time.DateTime;
+
 import common.EventI;
 import common.EventListI;
+import common.GroupI;
+import common.UserI;
+import common.WeekI;
 
 class Calendar extends JPanel implements ActionListener {
 
 	protected DayView mon, tue, wed, thu, fri, sat, sun;
 	protected EventListI eventList;
+	protected ArrayList<EventI> eventListByDay;
+	protected ArrayList<UserI> users;
+	protected ArrayList<GroupI> groups;
+	protected WeekI week;
+	protected DateTime date = new DateTime(2013,3,18,15,30);
 
 	public Calendar() {
+
+		users = new ArrayList<UserI>();
+		users.add(MainClass.getCurrentUser());
+
+		groups = new ArrayList<GroupI>();
+
+		try {
+			eventList = MainClass.sServer.eventStorage.getEventList(users,
+					groups, date);
+			eventListByDay = eventList.getList();
+		} catch (RemoteException e) {
+			throw new RuntimeException(e);
+		}
+
 		setPreferredSize(new Dimension(800, 400));
 		setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
@@ -37,32 +62,20 @@ class Calendar extends JPanel implements ActionListener {
 		add(sat);
 		add(sun);
 
+		addEvent();
+
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		System.out.println(e.getActionCommand().toString());
 	}
 
-	public void addEvents() throws RemoteException {
-		EventI[] eList = (EventI[]) eventList.getList().toArray();
-		for (int i = 0; i < eList.length; i++) {
-			int day = eList[i].getStart().getDayOfWeek();
-
-			if (day == 1) {
-				mon.model.addElement(eList[i]);
-			} else if (day == 2) {
-				tue.model.addElement(eList[i]);
-			} else if (day == 3) {
-				wed.model.addElement(eList[i]);
-			} else if (day == 4) {
-				thu.model.addElement(eList[i]);
-			} else if (day == 5) {
-				fri.model.addElement(eList[i]);
-			} else if (day == 6) {
-				sat.model.addElement(eList[i]);
-			} else if (day == 7) {
-				sun.model.addElement(eList[i]);
-			}
+	public void addEvent() {
+		for (int i = 0; i < eventListByDay.size(); i++) {
+			mon.model.addElement(eventListByDay.get(i));
 		}
+
+		// week.setWeek(year, weeknr)
+		// date = new DateTime(week.getYear(),week.getFromDate(),5,5,6);
 	}
 }
