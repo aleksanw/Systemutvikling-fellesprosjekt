e8c2a3de34e93bea70334411e9e278cf434ca954
Mads Paulsen <madslp@stud.ntnu.no>
2013-03-20 19:57:35 +0100

    extend stuff

--- src/common/EventsInvitedToI.java
+++ src/common/EventsInvitedToI.java
@@ -3,7 +3,7 @@ package common;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-public interface EventsInvitedToI {
+public interface EventsInvitedToI extends ListModelI {
 
 	public ArrayList<EventI> getEventList() throws RemoteException;
 	public ArrayList<InvitationI> getInvitationList() throws RemoteException;
